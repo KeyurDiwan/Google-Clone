c5a7f2afb181fc39f532ec99b81d8aa9ea1b9df3
keyur diwan <keyurdiwan009@gmail.com>
2022-02-01 15:19:18 +0530

    update avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Avatar = ({url}) => {
+const Avatar = ({url, className}) => {
     return <img
         loading='lazy'
-        className='rounded-full
+        className={`rounded-full
          w-10 h-10 
          cursor-pointer 
-         transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'
+         transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 ${className}`}
         src={url}
         alt='avatar'
     />
